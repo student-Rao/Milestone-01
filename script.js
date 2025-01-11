6497db5a6fb2785f05ef9beb5c47a6b93937d0fb
Rao Wakeel <160309352+student-Rao@users.noreply.github.com>
2025-01-11 20:30:54 +0500

    first commit

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ form.addEventListener('submit', function (event) {
     var education = document.getElementById('education').value;
     var experience = document.getElementById('experience').value;
     var skills = document.getElementById('skills').value;
-    var resumeHTML = "\n   <h2><b>Resume</b></h2>\n   <h3>Personal Information</h3>\n   <p><b>Name:</b>".concat(name, "</p>\n   <p><b>Name:</b>").concat(email, "</p>\n   <p><b>Name:</b>").concat(phone, "</p>\n   \n   <h3>Education</h3>\n   <p>").concat(education, "</p>\n   <h3>Experience</h3>\n   <p>").concat(education, "</p>\n   <h3>Skills</h3>\n   <p>").concat(skills, "</p>\n   \n   ");
+    var resumeHTML = "\n   <h2><b>Editable Resume</b></h2>\n   <h3>Personal Information</h3>\n   <p><b>Name:</b> <span contenteditable=\"true\">".concat(name, " </span></p>\n   <p><b>Email:</b>  <span contenteditable=\"true\">").concat(email, "</span></p>\n   <p><b>Phone Number:</b>  <span contenteditable=\"true\">").concat(phone, "</span></p>\n   \n   <h3>Education</h3>\n   <p>  <span contenteditable=\"true\">").concat(education, "</span></p>\n   <h3> Experience</h3>\n   <p> <span contenteditable=\"true\">").concat(education, "</span></p>\n   <h3>Skills</h3>\n   <p> <span contenteditable=\"true\">").concat(skills, "</span></p>\n   \n   ");
     if (resumeDisplayElement) {
         resumeDisplayElement.innerHTML = resumeHTML;
     }
--- script.ts
+++ script.ts
@@ -11,18 +11,18 @@ form.addEventListener('submit',(event:Event)=>{
     const experience=(document.getElementById('experience') as HTMLInputElement).value
     const skills=(document.getElementById('skills') as HTMLInputElement).value
    const resumeHTML=`
-   <h2><b>Resume</b></h2>
+   <h2><b>Editable Resume</b></h2>
    <h3>Personal Information</h3>
-   <p><b>Name:</b>${name}</p>
-   <p><b>Name:</b>${email}</p>
-   <p><b>Name:</b>${phone}</p>
+   <p><b>Name:</b> <span contenteditable="true">${name} </span></p>
+   <p><b>Email:</b>  <span contenteditable="true">${email}</span></p>
+   <p><b>Phone Number:</b>  <span contenteditable="true">${phone}</span></p>
    
    <h3>Education</h3>
-   <p>${education}</p>
-   <h3>Experience</h3>
-   <p>${education}</p>
+   <p>  <span contenteditable="true">${education}</span></p>
+   <h3> Experience</h3>
+   <p> <span contenteditable="true">${education}</span></p>
    <h3>Skills</h3>
-   <p>${skills}</p>
+   <p> <span contenteditable="true">${skills}</span></p>
    
    `;
    if(resumeDisplayElement){
